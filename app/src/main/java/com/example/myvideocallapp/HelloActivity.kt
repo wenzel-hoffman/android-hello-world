ca578421ea877e7304a0e2d6d488cb5f56835d72
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2023-04-20 02:17:19 +0300

    Make the app be minimal (just one activity and one layout)

---
+++ app/src/main/java/com/example/myvideocallapp/HelloActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myvideocallapp
+
+import android.Manifest
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HelloActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hello_screen)
+    }
+
+}
--- app/src/main/java/com/example/myvideocallapp/api/TokenResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myvideocallapp.api
-
-import androidx.annotation.Keep
-import com.google.gson.annotations.SerializedName
-
-@Keep
-data class TokenResponse(
-    @SerializedName("token") val token: String
-)
